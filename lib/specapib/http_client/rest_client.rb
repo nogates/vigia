bbe972c5bb1cd6f8101114b74f3463d52618c01d
David <nogates@sindominio.net>
2014-09-12 20:55:23 +1000

    use absolute name for class and format

--- lib/specapib/http_client/rest_client.rb
+++ lib/specapib/http_client/rest_client.rb
@@ -2,14 +2,17 @@ module SpecApib
   module HttpClient
     class RestClient
       attr_accessor :code, :headers, :body
+
       def initialize(http_options)
         @http_options = http_options
       end
+
       def run!
         perform_request
       end
-      
+
       private
+
       def parse_request(rest_client_result)
         {
           code: rest_client_result.code,
@@ -17,6 +20,7 @@ module SpecApib
           body: rest_client_result.body
         }
       end
+
       def error_request(exception)
         {
           code: exception.http_code, # Todo. Parse each exception
@@ -24,11 +28,12 @@ module SpecApib
           body: exception.response
         }
       end
+
       def perform_request
         begin
           request = ::RestClient::Request.execute(@http_options)
           parse_request(request)
-        rescue RestClient::Exception => exception
+        rescue ::RestClient::Exception => exception
           error_request(exception)
         end
       end
