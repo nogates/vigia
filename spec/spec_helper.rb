1a05c686f244339ab6b5cfdd22455b12f301e559
David Tapiador <david.tapiador@lonelyplanet.com.au>
2014-12-01 10:53:42 +0000

    tidy up spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,40 +1,9 @@
-# encoding: utf-8
+require 'simplecov'
 
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
-
-SimpleCov.minimum_coverage 100 # O_o
-SimpleCov.start do
-  add_filter "/spec/"
-end
+# Cucumber + Rspec should cover 100% of the gem
+SimpleCov.minimum_coverage 100
 
 require 'vigia'
 require 'pry'
 
 Dir["./spec/support/**/*.rb"].sort.each { |f| require f }
-
-module Vigia
-  class ExampleTest
-    class << self
-      def apib
-        RedSnow::parse apib_source
-      end
-
-      def apib_source
-        File.read apib_path
-      end
-
-      def apib_path
-        File.join __dir__, 'example.apib'
-      end
-
-      def server
-        Vigia::ExampleServer
-      end
-
-      def app
-        Vigia::ExampleApp
-      end
-    end
-  end
-end
