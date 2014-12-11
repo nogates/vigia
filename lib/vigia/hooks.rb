5fdc6d062e943ad41ae7d2d8c6e52e4e40216995
David Tapiador <david.tapiador@lonelyplanet.com.au>
2014-12-11 06:43:20 +0000

    move hooks to a module

---
+++ lib/vigia/hooks.rb
@@ -0,0 +1,46 @@
+module Vigia
+  module Hooks
+
+    def execute_hook(filter_name, rspec_context)
+      hooks_for_object(filter_name).each do |hook|
+        rspec_context.instance_exec(&hook)
+      end
+    end
+
+    def hooks_for_object(filter_name)
+      config_hooks(filter_name) + object_hooks(filter_name)
+    end
+
+    def with_hooks(rspec_context)
+      instance = self
+
+      rspec_context.before(:context) do
+        instance.execute_hook(:before, self)
+      end
+
+      rspec_context.after(:context) do
+        instance.execute_hook(:after, self)
+      end
+
+      instance.execute_hook(:extend, rspec_context)
+
+      yield
+    end
+
+    private
+
+    def object_hooks(filter_name)
+      return [] unless respond_to?(:options)
+      option_name = "#{ filter_name }_#{ self.class.name.split('::').last.downcase }".to_sym
+      [ *options[option_name] ].compact
+
+    end
+
+    def config_hooks(filter_name)
+      Vigia.config.hooks.each_with_object([]) do |hook, collection|
+        next unless self.is_a?(hook[:rspec_class]) and filter_name == hook[:filter]
+        collection << hook[:block]
+      end
+    end
+  end
+end
