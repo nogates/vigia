79043e6cbc77712a5681e139b92559ee74e05db3
David Tapiador <david.tapiador@lonelyplanet.com.au>
2014-12-11 06:27:33 +0000

    require pry first

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,7 @@ require 'simplecov'
 # Cucumber + Rspec should cover 100% of the gem
 SimpleCov.minimum_coverage 100
 
-require 'vigia'
 require 'pry'
+require 'vigia'
 
 Dir["./spec/support/**/*.rb"].sort.each { |f| require f }
