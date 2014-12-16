fd1795a456d885c55b9dff17868119c3336a0d9c
David Tapiador <david.tapiador@lonelyplanet.com.au>
2014-12-16 12:08:32 +1100

    improve default examples and test them

--- lib/vigia/sail/examples/default.rb
+++ lib/vigia/sail/examples/default.rb
@@ -1,11 +1,17 @@
-Vigia::Sail::Example.register(
-  :code_match,
-  description: 'has the expected HTTP code',
-  expectation: -> { expect(result.code).to be(expectations.code) }
-)
+ Vigia::Sail::Example.register(
+   :code_match,
+   description: 'has the expected HTTP code',
+   expectation: -> {
+     if expectations.code.is_a?(Range)
+       expect(expectations.code.member?(result.code)).to be true
+     else
+       expect(result.code).to be(expectations.code)
+     end
+   }
+ )
 
-Vigia::Sail::Example.register(
-  :include_headers,
-  description: 'includes the expected headers',
-  expectation: -> { expect(result[:headers]).to include(expectations[:headers]) }
-)
+ Vigia::Sail::Example.register(
+   :include_headers,
+   description: 'includes the expected headers',
+   expectation: -> { expect(result.headers).to include(expectations.headers) }
+ )
---
+++ spec/lib/sail/examples/default_spec.rb
@@ -0,0 +1,93 @@
+require 'spec_helper'
+
+describe 'Vigia default examples' do
+
+  let(:expectations) { double }
+  let(:result)       { double }
+
+  describe 'code_match' do
+    subject do
+      Vigia::Sail::Example.collection[:code_match]
+    end
+
+    context 'when the code is a range' do
+      before do
+        allow(expectations).to receive(:code).and_return(200..299)
+      end
+
+      context 'when the code is in the range' do
+        before do
+          allow(result).to receive(:code).and_return(201)
+        end
+
+        it 'passes' do
+          instance_exec(&subject[:expectation])
+        end
+      end
+
+      context 'when the code is not in the range' do
+        before do
+          allow(result).to receive(:code).and_return(301)
+        end
+
+        it 'fails' do
+          expect { instance_exec(&subject[:expectation]) }.to raise_error
+        end
+      end
+    end
+    context 'when the code is not range' do
+      before do
+        allow(expectations).to receive(:code).and_return(200)
+      end
+
+      context 'when the code is the same' do
+        before do
+          allow(result).to receive(:code).and_return(200)
+        end
+
+        it 'passes' do
+          instance_exec(&subject[:expectation])
+        end
+      end
+
+      context 'when the code is not the same' do
+        before do
+          allow(result).to receive(:code).and_return(201)
+        end
+
+        it 'fails' do
+          expect { instance_exec(&subject[:expectation]) }.to raise_error
+        end
+      end
+    end
+  end
+
+  describe 'include_headers' do
+    subject do
+      Vigia::Sail::Example.collection[:include_headers]
+    end
+
+    let(:expected_headers) { { a_header: 'its content' } }
+
+    before do
+      allow(expectations).to receive(:headers).and_return(expected_headers)
+      allow(result).to receive(:headers).and_return(result_headers)
+    end
+
+    context 'when the expected headers are included in the result' do
+      let(:result_headers) { { a_header: 'its content', another: 'another content' } }
+
+      it 'passes' do
+        instance_exec(&subject[:expectation])
+      end
+    end
+
+   context 'when the expected headers are not included in the result' do
+      let(:result_headers) { { another: 'another content' } }
+
+      it 'fails' do
+        expect { instance_exec(&subject[:expectation]) }.to raise_error
+      end
+    end
+  end
+end
